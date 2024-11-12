1cdee20031a9c5cfca4ae17993f91d2aa1010dc7
Subhasis Gouda <subhasisgouda999@gmail.com>
2024-11-12 07:30:19 +0530

    Initial commit: Add my javascript practice files

---
+++ Adding to a webpage/addition.js
@@ -0,0 +1,6 @@
+let items=["apple", "orange", "banana"]
+console.log(items)
+console.log(items.length)
+for(let i=0;i<items.length;i++){
+    console.log(items[i])
+}
---
+++ Syntax/basics.js
@@ -0,0 +1,9 @@
+/*This
+will teach us the basics of JS
+We will write some statements telling webpage what to do.
+*/
+
+let myvariable= 55
+let myVariable=65
+console.log(myvariable) //This will print 55 in the console
+alert(myVariable); //This will create a pop up in the webpage with 65 printed in it.
